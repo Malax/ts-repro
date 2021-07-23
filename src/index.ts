202093c2956c6e99cb6e3cf5aa76870e8dd8f2a0
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-07-23 19:03:26 +0200

    Initial

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// We can import Squirrel
+import { Squirrel, SalesforceFunction } from "sf-fx-sdk-nodejs"
+
+console.log("Here is code!");
+
+// This compiles, but shouldn't
+const a: SalesforceFunction = "qas"
